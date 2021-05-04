83ab28ff2465e55c174dbff6aa458919fde01ef8
Omar Azzam <omarazzam@Omars-MacBook-Pro.local>
2021-05-04 02:04:43 -0400

    Fix travis yaml container name

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,7 +2,11 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  try {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+  }catch (err) {
+    console.log("Error:" + err)
+  }
 });
