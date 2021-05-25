facd47f54dd498e1a103e59bb57c2583faf9a711
Omar Azzam <omar.azzam@ibm.com>
2021-05-25 17:35:15 -0400

    Updating title of the home page

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+           Welcome to my Fibonachi calculator!!
           </p>
           <a
             className="App-link"
