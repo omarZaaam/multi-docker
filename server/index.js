5295679c5d5764d7938467e5f2fca12f5b462545
Omar Azzam <omar.azzam@ibm.com>
2021-05-25 17:22:47 -0400

    Updating index.js for webhook test

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
